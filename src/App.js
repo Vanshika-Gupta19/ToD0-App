ea4f7a50225f4f869d4d86231155a9f752855c33
Vanshika-Gupta19 <vanshika2004.gupta19@gmail.com>
2024-06-19 16:18:15 +0530

    file added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useStyletron } from "baseui";
+import { useState } from 'react';
+import {Card} from 'baseui/card';
+import { StyledBody } from 'baseui/card';
+import { Input } from "baseui/input";
+import { Block } from "baseui/block";
+import { Button } from "baseui/button";
+import {List, arrayMove, arrayRemove} from "baseui/dnd-list"
 
-function App() {
+function App() { 
+  const [css, theme] = useStyletron();
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  
+  const addItems = ()=>{
+    if(inputValue.trim() !== ''){
+      setItems([...items , inputValue]);
+      setInputValue('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card 
+      className={css({
+        border: "2px solid black",
+        backgroundColor: "pink",
+        // background: theme.colors.backgroundLightPositive,
+        // border: theme.borders.border600
+      })}
+      >
+      <StyledBody>
+      <Block display={'flex'} alignItems={'center'} marginBottom={'20px'}>
+    <Input
+      value={inputValue}
+      onChange={e => setInputValue(e.target.value)}
+      placeholder="Enter Task"
+      clearOnEscape
+      overrides={{
+        Root: {
+          style: ({ $theme }) => ({ 
+            backgroundColor: $theme.colors.warning200,
+            width: '500px', 
+            marginRight: '50px'
+          })
+        },
+        InputContainer: {
+            style: ({ $theme }) => ({
+              outline: `${$theme.colors.warning200} solid`,
+              backgroundColor: $theme.colors.warning200
+            })
+        },
+      }}
+    />
+    <Button onClick={addItems}>Add Task</Button>
+    </Block>
+<List
+      items={items}
+      removable
+      onChange={({ oldIndex, newIndex }) =>
+        setItems(
+          newIndex === -1
+            ? arrayRemove(items, oldIndex)
+            : arrayMove(items, oldIndex, newIndex)
+        )
+      }
+      overrides={{
+        Label: {
+          style: ({ $theme }) => ({
+            color: $theme.colors.negative600
+          })
+        }
+      }}
+    />
+
+    </StyledBody>
+      </Card>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import {createRoot} from 'react-dom/client';
+import { Client as Styletron } from 'styletron-engine-atomic'; 
+import { Provider as StyletronProvider } from 'styletron-react';
+import { LightTheme, BaseProvider } from 'baseui';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const engine = new Styletron();
+const container = document.getElementById('root');
+const root = createRoot(container); // Using createRoot
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StyletronProvider value={engine}>
+    <BaseProvider theme={LightTheme}>
+      <App />
+    </BaseProvider>
+  </StyletronProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
